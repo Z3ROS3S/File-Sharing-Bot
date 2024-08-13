fa90e40e8cc2a6398ceb48260025c6dadd2d1d25
My name is Z <88902026+Z3ROS3S@users.noreply.github.com>
2024-08-13 16:48:23 -0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -9,9 +9,10 @@ from bot import Bot
 from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
 from helper_func import encode
 
+
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats']))
 async def channel_post(client: Client, message: Message):
-    reply_text = await message.reply_text("Tunggu yaaaaaa!", quote = True)
+    reply_text = await message.reply_text("Please Wait...!", quote = True)
     try:
         post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
     except FloodWait as e:
@@ -21,23 +22,12 @@ async def channel_post(client: Client, message: Message):
         print(e)
         await reply_text.edit_text("Something went Wrong..!")
         return
+
     converted_id = post_message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-
-    await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
-
-    if not DISABLE_CHANNEL_BUTTON:
-        try:
-            await post_message.edit_reply_markup(reply_markup)
-        except FloodWait as e:
-            await asyncio.sleep(e.value)
-            await post_message.edit_reply_markup(reply_markup)
-        except Exception:
-            pass
+    await reply_text.edit(f"{link}", disable_web_page_preview = True)
 
 @Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_ID))
 async def new_post(client: Client, message: Message):
@@ -49,12 +39,11 @@ async def new_post(client: Client, message: Message):
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    
-    # Tidak menambahkan tombol reply markup
+
     try:
-        await message.edit_reply_markup(None)  # Hapus tombol jika ada
+        await message.reply_text(f"{link}", disable_web_page_preview=True)
     except FloodWait as e:
         await asyncio.sleep(e.value)
-        await message.edit_reply_markup(None)
+        await message.reply_text(f"{link}", disable_web_page_preview=True)
     except Exception:
         pass
